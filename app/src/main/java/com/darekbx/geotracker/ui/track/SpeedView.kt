2e404ebdafaa41028826d58a0d93ccf0bd5ae0d6
DarekBx <darek.baranczuk@gmail.com>
2020-09-01 11:56:34 +0200

    Add zoom function to speed chart

--- app/src/main/java/com/darekbx/geotracker/ui/track/SpeedView.kt
+++ app/src/main/java/com/darekbx/geotracker/ui/track/SpeedView.kt
@@ -6,35 +6,59 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class SpeedView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
-    val chartPaint = Paint().apply {
+    private val chartPaint = Paint().apply {
         isAntiAlias = true
         color = Color.RED
     }
 
-    val guidePaint = Paint().apply {
+    private val zoomBackgroundPaint = Paint().apply {
+        isAntiAlias = true
+        color = Color.LTGRAY
+    }
+
+    private val guidePaint = Paint().apply {
         isAntiAlias = true
         color = Color.argb(50, 0, 0, 0)
     }
 
-    val textPaint = Paint().apply {
+    private val textPaint = Paint().apply {
         isAntiAlias = true
         color = Color.BLACK
         textSize = 24.0F
     }
 
-    val backgroundColor = Color.argb(10, 0, 0, 0)
-    val leftPadding = 96F
-    val topPadding = 18F
+    private val backgroundColor = Color.argb(10, 0, 0, 0)
+    private val leftPadding = 96F
+    private val topPadding = 18F
+
+    private var zoomPosition: Float? = null
 
     var values: List<Float> = emptyList()
         set(value) {
             field = value
             invalidate()
         }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_MOVE, MotionEvent.ACTION_DOWN -> {
+                zoomPosition = event?.x
+                invalidate()
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                zoomPosition = null
+                invalidate()
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -54,13 +78,69 @@ class SpeedView(context: Context, attributeSet: AttributeSet) : View(context, at
             val avgValue = values.average().toFloat()
             val widthRatio = width / count.toFloat()
             val heightRatio = height / maxValue
-            var firstPoint = PointF(leftPadding, height - values.first())
+            var firstPoint = PointF(leftPadding, height - (values.first() * heightRatio))
             val maxPosition = topPadding + (height - (maxValue * heightRatio))
             val avgPosition = topPadding + (height - (avgValue * heightRatio))
 
             drawGuide(canvas, maxPosition, maxValue)
             drawGuide(canvas, avgPosition, avgValue)
             drawValues(widthRatio, height, heightRatio, canvas, firstPoint)
+
+            drawZoom(widthRatio, heightRatio, canvas)
+        }
+    }
+
+    private fun drawZoom(
+        widthRatio: Float,
+        heightRatio: Float,
+        canvas: Canvas
+    ) {
+        zoomPosition?.let {
+
+            val height = getHeight().toFloat()
+            val zoomCount = 20
+            val position = (it / widthRatio).toInt()
+
+            if (zoomNotAvailable(zoomCount, position)) {
+                return
+            }
+
+            val zoomValues = mutableListOf<Float>().apply {
+                addAll(values.subList(position, position + (zoomCount / 2)))
+                addAll(values.subList(position - (zoomCount / 2), position))
+            }
+
+            val zoomWidthRatio = zoomCount
+            val zoomAreaStart = it - ((zoomCount / 2) * zoomWidthRatio)
+            val zoomAreaEnd = it + ((zoomCount / 2) * zoomWidthRatio)
+
+            var zoomFirstPoint = PointF(zoomAreaStart, height - (zoomValues.first() * heightRatio))
+
+            canvas.drawRect(
+                zoomAreaStart,
+                0F,
+                zoomAreaEnd,
+                getHeight().toFloat(),
+                zoomBackgroundPaint
+            )
+
+            zoomValues.forEachIndexed { index, value ->
+                if (index == 0) {
+                    return@forEachIndexed
+                }
+
+                val x = zoomAreaStart + (index * zoomWidthRatio)
+                val y = height - (value * heightRatio)
+
+                canvas.drawLine(
+                    zoomFirstPoint.x, zoomFirstPoint.y,
+                    x, y,
+                    chartPaint
+                )
+                zoomFirstPoint.x = x
+                zoomFirstPoint.y = y
+            }
+
         }
     }
 
@@ -88,6 +168,11 @@ class SpeedView(context: Context, attributeSet: AttributeSet) : View(context, at
             firstPoint.y = y
         }
     }
+
+    private fun zoomNotAvailable(zoomCount: Int, position: Int) =
+        values.size < zoomCount * 2
+                || position < zoomCount
+                || position + zoomCount >= values.size
 
     private fun drawGuide(canvas: Canvas, guideLinePosition: Float, value: Float) {
         canvas.drawLine(leftPadding, guideLinePosition, getWidth().toFloat(), guideLinePosition, guidePaint)
